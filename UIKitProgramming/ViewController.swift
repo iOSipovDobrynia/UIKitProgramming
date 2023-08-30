83b7024130f58723ca330743d393df9babc89954
iOSipovDobrynia <dobrynia.osipov@gmail.com>
2023-08-30 23:26:00 +0300

    add VC

--- UIKitProgramming/ViewController.swift
+++ UIKitProgramming/ViewController.swift
@@ -9,11 +9,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private let segmentedControl = UISegmentedControl()
+    private let mainLabel = UILabel()
+    private let slider = UISlider()
+    private let textField = UITextField()
+    private let datePicker = UIDatePicker()
+    private let switcher = UISwitch()
+    private let doneButton = UIButton()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupView()
     }
-
-
 }
 
+// MARK: - Settings View
+private extension ViewController {
+    func setupView() {
+        view.backgroundColor = .blue
+        
+    }
+}
+
+// MARK: - Settings
+private extension ViewController {
+    func addSubViews() {
+        [
+            segmentedControl,
+            mainLabel,
+            slider,
+            textField,
+            datePicker,
+            switcher,
+            doneButton
+        ].forEach { subView in
+            view.addSubview(subView)
+        }
+    }
+}
+
+// MARK: - Layout
+private extension ViewController {
+    func setupLayout() {
+        
+    }
+}
